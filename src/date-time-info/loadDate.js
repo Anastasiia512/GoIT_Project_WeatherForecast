2d5c2efbffd0b046f0173ef428054bff8e1c5f73
Anastasiia <nastyona221985@gmail.com>
2020-04-30 14:01:08 +0300

    modified

--- src/date-time-info/loadDate.js
+++ src/date-time-info/loadDate.js
@@ -1,9 +1,9 @@
 import * as moment from 'moment';
-import tz from 'moment-timezone';
 
 const refs = {
   infoMainDate: document.querySelector('[data-action="load-main-date"]'),
   infoMonth: document.querySelector('[data-action="load-month"]'),
+  infoTime: document.querySelector('[data-action="load-info-time"]'),
 };
 
 const dateInfo = function (day, date, month) {
@@ -11,15 +11,18 @@ const dateInfo = function (day, date, month) {
   refs.infoMonth.textContent = month;
 };
 
-export const timeInfo = function (parseData) {
-  if (!parseData.city.timezone) {
-    return;
-  }
-  const handleDateTimeInfo = function () {
-    const date = moment().format('DD ');
-    const day = moment().format('ddd');
-    const month = moment().format('MMMM');
-    dateInfo(day, date, month);
-  };
+const timeInfo = function () {
+  setInterval(() => {
+    const currentTime = moment().format('HH:mm:ss');
+    refs.infoTime.textContent = currentTime;
+  }, 1000);
+};
+
+const handleDateTimeInfo = function () {
+  const date = moment().format('DD ');
+  const day = moment().format('ddd');
+  const month = moment().format('MMMM');
+  dateInfo(day, date, month);
+  timeInfo();
 };
 handleDateTimeInfo();
